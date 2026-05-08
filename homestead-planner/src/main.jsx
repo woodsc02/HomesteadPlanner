7128d547b50b4f768585445eb1eef5ad66b6e32b
woodsc02 <woodscodyb@gmail.com>
2026-05-08 16:06:23 -0600

    Delete homestead-planner directory

--- homestead-planner/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import HomesteadPlanner from './HomesteadPlanner.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HomesteadPlanner />
-  </React.StrictMode>,
-)
